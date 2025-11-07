e9fc3ecd1b2b4e6441f51ccd09672a6a7ac77933
SmirnovDmitri <128578933+smirnovdmitri@users.noreply.github.com>
2025-11-07 12:05:58 +0300

    Add files via upload
    Changes after MaxHelp

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,36 +1,43 @@
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class SearchTests {
+
+    private String yaUrl = "https://ya.ru/";
+    private String siteName = "кинопоиск";
+    private SelenideElement inputYa = $("[name=text]");
+    private SelenideElement searchResaltYa = $("[data-cid='0']");
+    private String checkTextYa = "kinopoisk.ru";
+
 
     @Test
     void successfulSearchTest() {
         open("https://duckduckgo.com/");
         $("[id=searchbox_input]").setValue("yandex").pressEnter();
         $("[id=r1-1]").shouldHave(text("yandex.com"));
-        System.out.println();
-
     }
 
     @Test
     void successfulSearchTestGoogle() {
-        openSite();
-        $("[name=text]").setValue("duckduckgo").pressEnter();
-        checkResult();
-        System.out.println();
+        openSite(yaUrl);
+        search(inputYa, siteName);
+        checkResult(searchResaltYa, checkTextYa);
     }
 
-    void openSite() {
-        open("https://ya.ru/");
+    void openSite(String url) {
+        open(url);
     }
 
-    void checkResult() {
-        $("[data-fast=1]").shouldHave(text("duckduckgo.com"));
+    void search(SelenideElement locator, String value) {
+        locator.setValue(value).pressEnter();
     }
 
+    void checkResult (SelenideElement locator, String checkText) {
+        locator.shouldHave(text(checkText));
+    }
 }
-
