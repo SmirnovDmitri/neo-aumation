ffb3d7d008d49d6a936072c5420fd7e940d1699b
Dmitri Smirnov <dsmirnov@neoflex.ru>
2025-11-11 12:47:14 +0300

    Add files via upload
    Trying to commit all changes

--- src/test/java/SearchTestYa.java
+++ src/test/java/SearchTestYa.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SearchTestYa {
+    private String yaUrl = "https://ya.ru/";
+    private String siteName = "кинопоиск";
+    private SelenideElement inputYa = $("[name=text]");
+    private SelenideElement searchResaltYa = $("[data-cid='0']");
+    private String checkTextYa = "kinopoisk.ru";
+
+    @Test
+    void successfulSearchTestGoogle() {
+        openSite(yaUrl);
+        search(inputYa, siteName);
+        checkResult(searchResaltYa, checkTextYa);
+    }
+
+    void openSite(String url) {
+        open(url);
+    }
+
+    void search(SelenideElement locator, String value) {
+        locator.setValue(value).pressEnter();
+    }
+
+    void checkResult (SelenideElement locator, String checkText) {
+        locator.shouldHave(text(checkText));
+    }
 }
--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -8,36 +8,10 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class SearchTests {
 
-    private String yaUrl = "https://ya.ru/";
-    private String siteName = "кинопоиск";
-    private SelenideElement inputYa = $("[name=text]");
-    private SelenideElement searchResaltYa = $("[data-cid='0']");
-    private String checkTextYa = "kinopoisk.ru";
-
-
     @Test
     void successfulSearchTest() {
         open("https://duckduckgo.com/");
         $("[id=searchbox_input]").setValue("yandex").pressEnter();
         $("[id=r1-1]").shouldHave(text("yandex.com"));
-    }
-
-    @Test
-    void successfulSearchTestGoogle() {
-        openSite(yaUrl);
-        search(inputYa, siteName);
-        checkResult(searchResaltYa, checkTextYa);
-    }
-
-    void openSite(String url) {
-        open(url);
-    }
-
-    void search(SelenideElement locator, String value) {
-        locator.setValue(value).pressEnter();
-    }
-
-    void checkResult (SelenideElement locator, String checkText) {
-        locator.shouldHave(text(checkText));
     }
 }
