2871d8234fb73a6ca5d31b76f9ca79bb13aabf4f
Dmitri Smirnov <dsmirnov@neoflex.ru>
2025-11-13 22:40:55 +0300

    Add class example MyTestWithEC

---
+++ src/test/java/MyTestWithEC.java
@@ -0,0 +1,43 @@
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.codeborne.selenide.CollectionCondition.itemWithText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class MyTestWithEC {
+    private String yaUrl = "https://ya.ru/";
+    private String siteName = "кинопоиск";
+    private SelenideElement inputYa = $("[name=text]");
+    private SelenideElement searchResaltYa = $("[data-cid='0']");
+    private String checkTextYa = "kinopoisk.ru";
+
+    @Test
+    void successfulSearchTestGoogle() {
+        openSite(yaUrl);
+        search(inputYa, siteName);
+        checkResult(searchResaltYa, checkTextYa);
+        //использование ElementCollection
+        $$("#search-result").shouldHave(itemWithText("%ru.wikipedia.org›Кинопоиск%"));
+
+        int a=0;
+
+    }
+
+    void openSite(String url) {
+        open(url);
+    }
+
+    void search(SelenideElement locator, String value) {
+        locator.setValue(value).pressEnter();
+    }
+
+    void checkResult (SelenideElement locator, String checkText) {
+        locator.shouldHave(text(checkText));
+    }
+
+}
